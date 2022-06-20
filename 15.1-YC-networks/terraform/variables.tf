4e21e343acad4a4f09262e047a69ac002e4af95c
Vitaly Mozhaev <69746684+VitalyMozhaev@users.noreply.github.com>
2022-06-20 16:41:47 +0300

    Create variables.tf

---
+++ 15.1-YC-networks/terraform/variables.tf
@@ -0,0 +1,21 @@
+# variables
+
+variable "token" {
+  type = string
+  default = ""
+}
+
+variable "cloud_id" {
+  type = string
+  default = ""
+}
+
+variable "folder_id" {
+  type = string
+  default = ""
+}
+
+variable "zone" {
+  type = string
+  default = "ru-central1-a"
+}
